5c193e2a37eeecca0035e4434507651f66d7b568
CmBSinka <43815006+CmBSinka@users.noreply.github.com>
2023-01-25 11:40:14 +0300

    1

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=Yanis_Gorod',
+    'username' => 'Yanis_admin',
+    'password' => 'Admin11!',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
